84259b0aa1a3bc629dba93bdaecf09efbdfbcb81
AlfredVilorio <143658419+AlfredVilorio@users.noreply.github.com>
2023-09-10 00:51:04 +0000

    Pseudocodes

--- main.cs
+++ main.cs
@@ -2,16 +2,18 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-     
+     //Input "Enter inches"
     Console.WriteLine("Enter number of inches:  ");
+    
     string inchestyped = Console.ReadLine();
 
- 
+   // If input is decimal form, it gives the output and continues
     if (double.TryParse(inchestyped, out double inches))
      {
-     var x = inches * 2.54;  
+     var x = inches * 2.54; 
+    //Display "Enter inches" inches equal to var x or cm//
     Console.WriteLine($"{inches} inches is equal to {x} centimeters.");}
-    
+    // If input is not decimal form, output is invalid
    
     else {
       Console.WriteLine("Invalid");
