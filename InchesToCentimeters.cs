39545cebdcdbf355d21562dbf6f02641c780a868
AlfredVilorio <143658419+AlfredVilorio@users.noreply.github.com>
2023-09-11 21:11:00 +0000

    Changed Pseudocode and added some code

--- InchesToCentimeters.cs
+++ InchesToCentimeters.cs
@@ -2,7 +2,10 @@ using System;
 
 class Program {
   public static void Main (string[] args) {
-     //Input "Enter inches"
+    //Fixed decimal conversion
+    //1 inch = 2.54 centimeters
+    const double centimeters = 2.54;
+    //Input a number
     Console.WriteLine("Enter number of inches:  ");
     
     string inchestyped = Console.ReadLine();
@@ -10,9 +13,10 @@ class Program {
    // If input is decimal form, it gives the output and continues
     if (double.TryParse(inchestyped, out double inches))
      {
-     var x = inches * 2.54; 
+    
+     var x = inches * centimeters; 
     //Displays conversion of inches to centimeters//
-    Console.WriteLine($"{inches} inches is equal to {x} centimeters.");}
+    Console.WriteLine($"{inches} inches is {x} centimeters.");}
     // If input is not decimal form, output is invalid
    
     else {
